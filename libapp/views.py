378a2b445cadafd6b303c141a8bc6b0abfb1402e
shimaa khallaf <shimaakhalaf>
2023-02-21 13:30:48 +0200

    gitting books from database

--- libapp/views.py
+++ libapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
-    return render(request , 'pages/index.html')
+    books = Book.objects.all()
+    return render(request , 'pages/index.html' , {"books":books})
 def books(request):
     return render(request , 'pages/books.html')
 def delete(request):
