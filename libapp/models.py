645c01ada72c90f46a102f23392c3870f94873dc
shimaa khallaf <shimaakhalaf>
2023-02-21 12:33:53 +0200

    adding media settings

--- libapp/models.py
+++ libapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 class Book(models.Model):
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250 , blank=True , null=True)
@@ -15,3 +17,5 @@ class Book(models.Model):
     rental_period = models.IntegerField(blank=True , null=True)
     activity = models.BooleanField(default=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT , null=True , blank=True)
+    def __str__(self):
+        return self.title
