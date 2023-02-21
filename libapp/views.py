0022b779987b0e81a5c18cb38ad26b226d94a115
shimaa khallaf <shimaakhalaf>
2023-02-21 14:13:26 +0200

    retrieving books from DB dynamically

--- libapp/views.py
+++ libapp/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def index(request):
+    categories = Category.objects.all()
     books = Book.objects.all()
-    return render(request , 'pages/index.html' , {"books":books})
+    return render(request , 'pages/index.html' , {"books":books , "categories":categories})
 def books(request):
-    return render(request , 'pages/books.html')
+    books = Book.objects.all()
+    categories = Category.objects.all()
+    return render(request , 'pages/books.html' ,{"books":books , "categories":categories})
 def delete(request):
-    return render(request , 'pages/delete.html')
+    categories = Category.objects.all()
+    return render(request , 'pages/delete.html' , { "categories":categories})
 def update(request):
-    return render(request , 'pages/update.html')
+    categories = Category.objects.all()
+    return render(request , 'pages/update.html' , { "categories":categories})
